d67cb94f45cf9ea4bd7dd743a3a011bcf29b20d1
Juan Pablo Allende <juanpabloallende2018@gmail.com>
2021-02-19 11:08:37 -0300

    Initial commit

---
+++ src/main/java/git/juampa99/half_url/HalfUrlApplication.java
@@ -0,0 +1,13 @@
+package git.juampa99.half_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalfUrlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HalfUrlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/git/juampa99/half_url/HalfUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package git.juampa99.half_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalfUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
